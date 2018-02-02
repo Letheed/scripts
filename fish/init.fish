7df461bc51e573dd588a3d69af468e182861b4fe
letheed <letheed@outlook.com>
2018-02-02 04:51:23 +0100

    fish: init.fish

---
+++ fish/init.fish
@@ -0,0 +1,26 @@
+# Git prompt
+set -g __fish_git_prompt_show_informative_status 'yes'
+set -g __fish_git_prompt_showdirtystate 'yes'
+set -g __fish_git_prompt_showstashstate 'yes'
+set -g __fish_git_prompt_showuntrackedfiles 'yes'
+set -g __fish_git_prompt_showcolorhints 'yes'
+set -g __fish_git_prompt_showupstream 'verbose name'
+
+# Width for tabulations
+tabs -4
+
+
+### Oh-my-fish ###
+
+# bobthefish theme
+#set -g theme_display_user yes
+set -g default_user damien # implies theme_display_user
+set -g theme_display_cmd_duration yes
+set -g theme_show_exit_status yes
+set -g theme_title_display_process yes
+set -g theme_date_format "+%R %a %d/%m/%y"
+set -g theme_display_git_ahead_verbose yes
+set -g theme_git_worktree_support yes
+
+# GRC plugin
+set -g grcplugin_ls --color
